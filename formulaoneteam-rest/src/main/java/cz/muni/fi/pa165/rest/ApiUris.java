4f46167b1a6d9d6420388578b1638d57452069c8
mrnustik <michal.mrnustik@outlook.com>
2018-12-12 19:31:00 +0100

    Disable ApiUris creation (#89)
    * Make ApiUris abstract
    
    * Use private constructor instead of abstracting the class
    
    * Move constructor after constants

--- formulaoneteam-rest/src/main/java/cz/muni/fi/pa165/rest/ApiUris.java
+++ formulaoneteam-rest/src/main/java/cz/muni/fi/pa165/rest/ApiUris.java
@@ -4,8 +4,12 @@ package cz.muni.fi.pa165.rest;
  * @author mrnda (Michal Mrnuštík)
  */
 
-public class ApiUris {
+public final class ApiUris {
     public static final String ROOT_URI_RACE = "/race";
     public static final String ROOT_URI_TEST_DRIVE = "/test-drive";
     public static final String ROOT_URI_RACE_PARTICIPATION = "/race-participation";
+    
+    private ApiUris() {
+        throw new IllegalAccessError("This constructor should never get called.");
+    }
 }
