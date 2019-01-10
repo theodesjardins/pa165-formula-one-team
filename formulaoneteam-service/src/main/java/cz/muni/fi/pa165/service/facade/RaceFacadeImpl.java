3532bac3ef6482b0c6920bb1040773417d63bed0
Michal Mrnuštík <michal.mrnustik@outlook.com>
2019-01-10 14:35:11 +0100

    Fix constraint violation when removing race entity
    Signed-off-by: Michal Mrnuštík <michal.mrnustik@outlook.com>

--- formulaoneteam-service/src/main/java/cz/muni/fi/pa165/service/facade/RaceFacadeImpl.java
+++ formulaoneteam-service/src/main/java/cz/muni/fi/pa165/service/facade/RaceFacadeImpl.java
@@ -4,6 +4,7 @@ import cz.muni.fi.pa165.dto.race.RaceDTO;
 import cz.muni.fi.pa165.entity.Race;
 import cz.muni.fi.pa165.facade.RaceFacade;
 import cz.muni.fi.pa165.service.RaceService;
+import cz.muni.fi.pa165.service.exceptions.FormulaOneTeamException;
 import cz.muni.fi.pa165.service.facade.base.EntityFacadeImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class RaceFacadeImpl extends EntityFacadeImpl<RaceDTO, Race, RaceService> implements RaceFacade {
+
+    @Override
+    public void remove(long id) {
+        final Race race = service.findById(id);
+        if (!race.getRaceParticipations().isEmpty()) {
+            throw new FormulaOneTeamException("Race can't be deleted because there are drivers participating in it.");
+        }
+        super.remove(id);
+    }
 
     @Override
     protected Class<RaceDTO> getDtoClass() {
